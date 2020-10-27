6b61f7d3c30708b1e2f51510998af5ea83a63389
TJM-UU <72815028+TJM-UU@users.noreply.github.com>
2020-10-27 22:59:46 +0100

    ellips kader en gevulde ellips toegevoegd
    Icoontjes zijn nog wel nodig, en misschien andere namen.

--- SchetsEditor/SchetsWin.cs
+++ SchetsEditor/SchetsWin.cs
@@ -47,6 +47,8 @@ namespace SchetsEditor
                                     , new LijnTool()
                                     , new RechthoekTool()
                                     , new VolRechthoekTool()
+                                    , new EllipsTool()
+                                    , new VolEllipsTool()
                                     , new TekstTool()
                                     , new GumTool()
                                     };
--- SchetsEditor/Tools.cs
+++ SchetsEditor/Tools.cs
@@ -104,6 +104,27 @@ namespace SchetsEditor
         {   g.FillRectangle(kwast, TweepuntTool.Punten2Rechthoek(p1, p2));
         }
     }
+    
+    public class EllipsTool : TweepuntTool
+    {
+        public override string ToString() { return "ellips kader"; }
+
+        public override void Bezig(Graphics g, Point p1, Point p2)
+        {
+            g.DrawEllipse(MaakPen(kwast, 3), TweepuntTool.Punten2Rechthoek(p1, p2));
+        }
+    }
+
+    public class VolEllipsTool : EllipsTool
+    {
+        public override string ToString() { return "gevulde ellips"; }
+
+        public override void Compleet(Graphics g, Point p1, Point p2)
+        {
+            g.FillEllipse(kwast, TweepuntTool.Punten2Rechthoek(p1, p2));
+        }
+    }
+
 
     public class LijnTool : TweepuntTool
     {
